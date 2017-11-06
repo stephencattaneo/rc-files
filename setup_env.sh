d846185fad4e1e85589afe8197a2a0ec1d4c2fe9
Stephen Cattaneo <$EMAIL_ACCOUNT>
2017-11-06 14:43:54 -0800

    vimrc

--- setup_env.sh
+++ setup_env.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 function sublime {
-SUBL_PATH='/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl'
-if [ -e "$SUBL_PATH" ]
-then
-  mkdir $HOME/bin
-  cd $HOME/bin
-  ln -sf "$SUBL_PATH" subl
-else
-  echo 'Could not find `subl` at this path:'
-  echo $SUBL_PATH
-fi
+  SUBL_PATH='/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl'
+  if [ -e "$SUBL_PATH" ]
+  then
+    mkdir $HOME/bin
+    cd $HOME/bin
+    ln -sf "$SUBL_PATH" subl
+  else
+    echo 'Could not find `subl` at this path:'
+    echo $SUBL_PATH
+  fi
 }
 
 function aliases {
   cd
   ln -sf $WORKSPACE/rc-files/bashrc .bash_profile
 
-  for i in gitconfig pryrc psqlrc screenrc
+  for i in gitconfig pryrc psqlrc screenrc vimrc
   do
     ln -sf $WORKSPACE/rc-files/$i .$i
   done
---
+++ vimrc
@@ -0,0 +1,5 @@
+:set nu
+" set nonu to turn off
+:set laststatus=2
+:set statusline+=%F\ %l\:%c
+:set shiftwidth=2
